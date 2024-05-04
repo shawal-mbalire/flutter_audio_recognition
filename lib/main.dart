a3e5a7de7d0efe4bebe31a4aa5093a8d146b866d
shawal-mbalire <mbalireshawal@gmail.com>
2024-05-04 17:03:25 +0300

    saturday

--- lib/main.dart
+++ lib/main.dart
@@ -1,202 +1,56 @@
-import 'dart:async';
-import 'dart:convert';
-import 'dart:developer';
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:tflite_audio/tflite_audio.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  final isRecording = ValueNotifier<bool>(false);
-  Stream<Map<dynamic, dynamic>>? result;
-
-  final String model = 'assets/chainsaw_detection.tflite';
-  final String label = 'assets/labels.txt';
-  final String inputType = 'rawAudio';
-  final String audioDirectory = 'assets/chainsaw.wav';
-  final int sampleRate = 44100;
-  final int bufferSize = 11016;
-  final bool outputRawScores = false;
-  final int numOfInferences = 5;
-  final int numThreads = 1;
-  final bool isAsset = true;
-  final double detectionThreshold = 0.3;
-  final int averageWindowDuration = 1000;
-  final int minimumTimeBetweenSamples = 30;
-  final int suppressionTime = 1500;
-
   @override
   void initState() {
     super.initState();
     TfliteAudio.loadModel(
-      inputType: inputType,
-      model: model,
-      label: label,
+      // numThreads: this.numThreads,
+      // isAsset: this.isAsset,
+      // outputRawScores: outputRawScores,
+      inputType: 'decidedWav',
+      model: 'assets/chainsaw_detection.tflite',
+      label: 'assets/label.txt',
     );
-    TfliteAudio.setSpectrogramParameters(nMFCC: 40, hopLength: 16384);
   }
 
-  void getResult() {
-    result = TfliteAudio.startAudioRecognition(
+  String getPrediction() {
+    dynamic result = TfliteAudio.startAudioRecognition(
       sampleRate: sampleRate,
       bufferSize: bufferSize,
-      numOfInferences: numOfInferences,
     );
-
-    result
-        ?.listen((event) =>
-            log("Recognition Result: " + event["recognitionResult"].toString()))
-        .onDone(() => isRecording.value = false);
+    return result.toString();
   }
-
-  ///fetches the labels from the text file in assets
-  Future<List<String>> fetchLabelList() async {
-    List<String> _labelList = [];
-    await rootBundle.loadString(this.label).then((q) {
-      for (String i in const LineSplitter().convert(q)) {
-        _labelList.add(i);
-      }
-    });
-    return _labelList;
-  }
-
-  ///handles null exception if snapshot is null.
-  String showResult(AsyncSnapshot snapshot, String key) =>
-      snapshot.hasData ? snapshot.data[key].toString() : '0 ';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            key: _scaffoldKey,
-            appBar: AppBar(
-              title: const Text('Chainsaw Detections'),
-            ),
-
-            ///Streambuilder for inference results
-            body: StreamBuilder<Map<dynamic, dynamic>>(
-                stream: result,
-                builder: (BuildContext context,
-                    AsyncSnapshot<Map<dynamic, dynamic>> inferenceSnapshot) {
-                  ///futurebuilder for getting the label list
-                  return FutureBuilder(
-                      future: fetchLabelList(),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<List<String>> labelSnapshot) {
-                        switch (inferenceSnapshot.connectionState) {
-                          case ConnectionState.none:
-                            //Loads the asset file.
-                            if (labelSnapshot.hasData) {
-                              return labelListWidget(labelSnapshot.data);
-                            } else {
-                              return const CircularProgressIndicator();
-                            }
-                          case ConnectionState.waiting:
-
-                            ///Widets will let the user know that its loading when waiting for results
-                            return Stack(children: <Widget>[
-                              Align(
-                                  alignment: Alignment.bottomRight,
-                                  child: inferenceTimeWidget('calculating..')),
-                              labelListWidget(labelSnapshot.data),
-                            ]);
-
-                          ///Widgets will display the final results.
-                          default:
-                            return Stack(children: <Widget>[
-                              Align(
-                                  alignment: Alignment.bottomRight,
-                                  child: inferenceTimeWidget(showResult(
-                                          inferenceSnapshot, 'inferenceTime') +
-                                      'ms')),
-                              labelListWidget(
-                                  labelSnapshot.data,
-                                  showResult(
-                                      inferenceSnapshot, 'recognitionResult'))
-                            ]);
-                        }
-                      });
-                }),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerFloat,
-            floatingActionButton: ValueListenableBuilder(
-                valueListenable: isRecording,
-                builder: (context, value, widget) {
-                  if (value == false) {
-                    return FloatingActionButton(
-                      onPressed: () {
-                        isRecording.value = true;
-                        setState(() {
-                          getResult();
-                        });
-                      },
-                      backgroundColor: Colors.blue,
-                      child: const Icon(Icons.mic),
-                    );
-                  } else {
-                    return FloatingActionButton(
-                      onPressed: () {
-                        log('Audio Recognition Stopped');
-                        TfliteAudio.stopAudioRecognition();
-                      },
-                      backgroundColor: Colors.red,
-                      child: const Icon(Icons.adjust),
-                    );
-                  }
-                })));
-  }
-
-  ///If snapshot data matches the label, it will change colour
-  Widget labelListWidget(List<String>? labelList, [String? result]) {
-    return Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: labelList!.map((labels) {
-              if (labels == result) {
-                return Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Text(labels.toString(),
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                          color: Colors.green,
-                        )));
-              } else {
-                return Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Text(labels.toString(),
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        )));
-              }
-            }).toList()));
-  }
-
-  ///If the future isn't completed, shows 'calculating'. Else shows inference time.
-  Widget inferenceTimeWidget(String result) {
-    return Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(result,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.black,
-            )));
+    return firstPage;
   }
 }
+
+MaterialApp firstPage = MaterialApp(
+  title: 'Chainsaw',
+  debugShowCheckedModeBanner: false,
+  theme: ThemeData(
+      primarySwatch: Colors.blue,
+      brightness: Brightness.dark,
+      primaryColor: Colors.amber),
+  home: Scaffold(
+    appBar: AppBar(
+      title: const Text('Chainsaw detection'),
+    ),
+    body: const Center(
+      child: Text('Chainsaw'),
+    ),
+  ),
+);
